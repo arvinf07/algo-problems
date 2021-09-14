a048b579f92e8b624a8c2de4a300ddbfffc8735c
Arvin Fernandez <arvinf07@gmail.com>
2021-09-14 16:40:29 -0400

    passing all tests

--- CodeSignal/palindromeRearranging.js
+++ CodeSignal/palindromeRearranging.js
@@ -1,6 +1,4 @@
 function palindromeRearranging(inputString) {
-  if(inputString.length < 3) return true
-  
   let stringObj = {}
   
   for(let i = 0; i < inputString.length; i++){
@@ -10,19 +8,16 @@ function palindromeRearranging(inputString) {
           stringObj[inputString[i]] = 1
       }
   }
-  console.log(stringObj)
   
   let keys = Object.keys(stringObj)
-  let singleLetters = 0 
+  let odds = 0 
   
   for(let i = 0; i < keys.length; i++){
-      if(stringObj[keys[i]] === 1) {
-          singleLetters++ 
-      }
+      if(stringObj[keys[i]] % 2 !== 0) {
+          odds++ 
+      } 
       
-      if(stringObj[keys[i]] !== 1 && stringObj[keys[i]]%2 !== 0 || singleLetters > 1) return false
-      
-
+      if(odds > 1) return false
   }
   
   return true
