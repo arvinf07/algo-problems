61b39f9eff9d50ef278f8ff3dc0ff1dbba017559
Arvin Fernandez <arvinf07@gmail.com>
2021-09-19 15:58:50 -0400

    solved!

--- CodeSignal/reverseInParentheses.js
+++ CodeSignal/reverseInParentheses.js
@@ -1,5 +1,13 @@
 function reverseInParentheses(inputString) {
-  // initialize a returnArr var
-  // convert the string to an array
-  // iterate through the arrau
+  var s = inputString;
+
+  while (s.indexOf(")",0)!=-1) {
+    var c = s.indexOf(")", 0);
+    var a = s.lastIndexOf("(", c);
+    var b = s.slice(a + 1, c).split("").reverse().join("");
+    s = s.slice(0, a) + b + s.slice(c + 1);
+
+    console.log(c,a,b,s);
+  }
+  return s;
 }
