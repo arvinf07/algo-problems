1e8fbe4efa41d0147bb7f69ccfbfd589a6880456
Arvin Fernandez <arvinf07@gmail.com>
2021-10-08 15:56:31 -0400

    solved with constant time!!

--- CodeSignal/equallyStrong.js
+++ CodeSignal/equallyStrong.js
@@ -1,3 +1,11 @@
 function areEquallyStrong(yourLeft, yourRight, friendsLeft, friendsRight) {
-  return (yourLeft + yourRight === friendsLeft + friendsRight)
+  if(yourRight === friendsRight){
+      if(yourLeft === friendsLeft) return true
+  }
+  
+  if(yourRight === friendsLeft){
+      if(yourLeft === friendsRight) return true
+  }
+  
+  return false
 }
