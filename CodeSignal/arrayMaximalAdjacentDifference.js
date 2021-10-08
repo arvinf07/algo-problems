8e32e1f78b74a2840d000c23606ec7ca9febca58
Arvin Fernandez <arvinf07@gmail.com>
2021-10-08 16:28:07 -0400

    passed all tests O(n)

--- CodeSignal/arrayMaximalAdjacentDifference.js
+++ CodeSignal/arrayMaximalAdjacentDifference.js
@@ -1,3 +1,16 @@
 function arrayMaximalAdjacentDifference(inputArray) {
+  let differenceVar = 0
+  
+  for(let i = 0; i < inputArray.length; i++){
+      let prevDiff = inputArray[i] - inputArray[i - 1]
+      let nextDiff = inputArray[i] - inputArray[i + 1]
+      
+      if(differenceVar < prevDiff) differenceVar = prevDiff
+      
+      if(differenceVar < nextDiff) differenceVar = nextDiff
+      
+  }
+  
+  return differenceVar
 
 }
